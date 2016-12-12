ab2c16285e275fd938bab9dd0401a36a6fc9ed0e
simov <simeonvelichkov@gmail.com>
2016-12-12 18:05:32 +0200

    Add route for querying events data

--- server/api.js
+++ server/api.js
@@ -6,6 +6,17 @@ var uuid = require('uuid')
 module.exports = (m) => {
   var api = express()
 
+  api.get('/events', (req, res) => {
+    m.events.findAll({where: req.query}).then((events) => {
+      res.writeHead(200, {'content-type': 'application/json'})
+      res.end(JSON.stringify({body: events}))
+    })
+    .catch((err) => {
+      res.writeHead(400, {'content-type': 'application/json'})
+      res.end(JSON.stringify({body: err.message}))
+    })
+  })
+
   api.post('/events', (req, res) => {
     req.body.id = uuid.v1()
     m.events.create(req.body).then((event) => {
--- test/events.js
+++ test/events.js
@@ -7,6 +7,9 @@ var path = require('path')
 var request = require('request')
 var _import = require('../schema/import')
 var config = require('../config/server')[env]
+var fixtures = {
+  events: require('./fixtures/events')
+}
 
 
 describe('events', () => {
@@ -20,6 +23,24 @@ describe('events', () => {
           done()
         }
       })
+    })
+  })
+
+  it('get events', (done) => {
+    request({
+      method: 'GET',
+      url: 'http://localhost:' + config.port + '/api/events',
+      qs: {
+        created_at: {$gt: '2016-12-12 15:11:14.513 +00:00'}
+      },
+      callback: (err, res, body) => {
+        if (err) {
+          done(err)
+          return
+        }
+        t.deepEqual(JSON.parse(body), fixtures.events)
+        done()
+      }
     })
   })
 
@@ -41,7 +62,23 @@ describe('events', () => {
           return
         }
         t.deepEqual(body, {body: 'ok'})
-        done()
+        request({
+          method: 'GET',
+          url: 'http://localhost:' + config.port + '/api/events',
+          qs: {
+            title: 'hey'
+          },
+          callback: (err, res, body) => {
+            if (err) {
+              done(err)
+              return
+            }
+            var events = JSON.parse(body).body
+            t.equal(events.length, 1)
+            t.equal(events[0].title, 'hey')
+            done()
+          }
+        })
       }
     })
   })
