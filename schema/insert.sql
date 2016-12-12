8560b1355bea372f47e2fc8142473dfb6548ea92
simov <simeonvelichkov@gmail.com>
2016-12-12 17:39:44 +0200

    Add test fixture

---
+++ schema/insert.sql
@@ -0,0 +1,13 @@
+
+-- events
+insert into events (id, title, body, source, author, created_at, updated_at)
+  values ('123', 'title1', 'body1', 'fixture', 'script',
+    '2016-12-12 15:11:14.513 +00:00', '2016-12-12 15:11:14.513 +00:00') ;
+
+insert into events (id, title, body, source, author, created_at, updated_at)
+  values ('124', 'title2', 'body2', 'fixture', 'script',
+    '2016-12-12 15:21:14.513 +00:00', '2016-12-12 15:21:14.513 +00:00') ;
+
+insert into events (id, title, body, source, author, created_at, updated_at)
+  values ('125', 'title3', 'body3', 'fixture', 'script',
+    '2016-12-12 15:25:14.513 +00:00', '2016-12-12 15:25:14.513 +00:00') ;
